da929559b878de58bd6dfef960fc0cdfc2ff0899
Eduardo Neira <edun327@gmail.com>
2019-02-15 01:25:57 -0300

    Added stop callback to SignalHandler

--- big_fiubrother_core/signal_handler.py
+++ big_fiubrother_core/signal_handler.py
@@ -3,11 +3,15 @@ import logging
 
 class SignalHandler:
 
-    def __init__(self):
+    def __init__(self, stop_callback=None):
+        self.stop_callback = stop_callback
         self.stop_signal_received = False
         signal.signal(signal.SIGINT, self.__stop_signal_received)
         signal.signal(signal.SIGTERM, self.__stop_signal_received)
 
     def __stop_signal_received(self, signum, frame):
+        if not (self.stop_callback is None):
+            self.stop_callback()
+
         self.stop_signal_received = True
         logging.info('Signal to stop received')

8206ac3a8bc6b9db3b42dd1a03bd46241bc99cd8
Gabriel <ga-yo-so@hotmail.com>
2019-06-13 20:58:51 -0300

    trick to increase fps: only calculates embedding once every many frames, or when amount of bboxes changed since last frame. on all other cases, compares bbox centroids between frames to assign old ids to the new frame

--- big_fiubrother_core/face_embedding_worker.py
+++ big_fiubrother_core/face_embedding_worker.py
@@ -1,9 +1,8 @@
 from big_fiubrother_core.messages import FaceEmbeddingMessage, FaceClassificationMessage, MessageClientFactory
 from big_fiubrother_classification.face_embedder_factory import FaceEmbedderFactory
+from big_fiubrother_core.async_task import AsyncTask
 import sys
-import cv2
 import yaml
-import numpy as np
 
 
 class FaceEmbeddingWorker:
@@ -22,6 +21,10 @@ class FaceEmbeddingWorker:
 
         # Create Face Embedder
         self.face_embedder = FaceEmbedderFactory.build(config_file_path)
+
+        # Save last result for centroid comparison
+        self.previous_message = None
+        self.centroid_use_counter = 0
 
     def start(self):
 
@@ -42,21 +45,53 @@ class FaceEmbeddingWorker:
         face_embedding_message = FaceEmbeddingMessage.decode(message_bytes)
 
         face_embeddings = []
-        for face_box in face_embedding_message.face_boxes:
-            # Get face from frame
-            frame = face_embedding_message.frame_bytes
-            face = frame[face_box[1]:face_box[3], face_box[0]:face_box[2]]
+        if len(face_embedding_message.face_boxes) != 0:
+            if self.centroid_use_counter % 100 == 0 or \
+                    len(face_embedding_message.face_boxes) != len(self.previous_message.face_boxes):
 
-            # Perform face classification
-            embedding = self.face_embedder.get_embedding_mem(face)
-            face_embeddings.append(embedding)
+                print("Embedding")
+                self.centroid_use_counter = 1
+                for face_box in face_embedding_message.face_boxes:
+                    # Get face from frame
+                    frame = face_embedding_message.frame_bytes
+                    face = frame[face_box[1]:face_box[3], face_box[0]:face_box[2]]
+
+                    # Perform face classification
+                    embedding = self.face_embedder.get_embedding_mem(face)
+                    face_embeddings.append(embedding)
+
+            else:
+                #print("Centroid")
+                face_box_min_distances = []
+
+                for i in range(len(face_embedding_message.face_boxes)):
+                    face_embeddings.append(0)
+                    face_box_min_distances.append(999)
+
+                    face_box = face_embedding_message.face_boxes[i]
+                    face_box_x = (face_box[0] + face_box[2]) / 2
+                    face_box_y = (face_box[1] + face_box[3]) / 2
+
+                    for j in range(len(self.previous_message.face_boxes)):
+                        prev_face_box = self.previous_message.face_boxes[j]
+                        prev_face_box_x = (prev_face_box[0] + prev_face_box[2]) / 2
+                        prev_face_box_y = (prev_face_box[1] + prev_face_box[3]) / 2
+
+                        dist = ((face_box_x - prev_face_box_x)**2 + (face_box_y - prev_face_box_y)**2)**0.5
+
+                        if dist < face_box_min_distances[i]:
+                            face_box_min_distances[i] = dist
+                            face_embeddings[i] = self.previous_message.face_embeddings[j]
 
         # Post face embedding job request
-        face_classification_message = FaceClassificationMessage(face_embedding_message.frame_id,
+        face_classification_message = FaceClassificationMessage(face_embedding_message.camera_id,
+                                                                face_embedding_message.timestamp,
+                                                                face_embedding_message.frame_id,
                                                                 face_embedding_message.frame_bytes,
                                                                 face_embedding_message.face_boxes,
                                                                 face_embeddings)
         self.message_publisher.publish(face_classification_message.encode())
+        self.previous_message = face_classification_message
 
 
 if __name__ == "__main__":

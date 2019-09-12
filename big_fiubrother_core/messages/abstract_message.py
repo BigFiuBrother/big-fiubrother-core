6e266024f7fa55a019417c3bb6085d0099ef0f60
Eduardo Neira <edun327@gmail.com>
2019-09-12 20:26:24 -0300

    Messages have a type

--- big_fiubrother_core/messages/abstract_message.py
+++ big_fiubrother_core/messages/abstract_message.py
@@ -1,11 +1,13 @@
 import pickle
 
 
+def decode(data):
+    return pickle.loads(data)
+
 class AbstractMessage:
+
+    def type(self):
+        return __class__.__name__
 
     def encode(self):
         return pickle.dumps(self)
-
-    @staticmethod
-    def decode(data):
-        return pickle.loads(data)

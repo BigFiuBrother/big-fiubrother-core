2a4f925fd02a700beeb9604f13b5112ecb64bb33
Eduardo Neira <edun327@gmail.com>
2019-09-25 22:44:10 -0300

    Update to version and gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='big-fiubrother-core',
-   version='0.1.1',
+   version='0.2.0',
    description='Big Fiubrother Core Utilities',
    license="GPLv3",
    long_description=long_description,

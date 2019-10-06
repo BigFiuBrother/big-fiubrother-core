044c9184c1cdf6f52fdb7926faf26f6e3baacd96
Eduardo Neira <edun327@gmail.com>
2019-10-05 22:31:31 -0300

    Change from queue to routing key in publisher

--- big_fiubrother_core/message_clients/rabbitmq/publisher.py
+++ big_fiubrother_core/message_clients/rabbitmq/publisher.py
@@ -6,7 +6,7 @@ class Publisher:
     def __init__(self, configuration):
         self.host = configuration['host']
         self.exchange = configuration['exchange']
-        self.queue = configuration['queue']
+        self.routing_key = configuration['routing_key']
         
         self.credentials = pika.PlainCredentials(configuration['username'], configuration['password'])
         self.parameters = pika.ConnectionParameters(host=self.host, credentials=self.credentials)
@@ -14,4 +14,4 @@ class Publisher:
         self.channel = self.connection.channel()
 
     def publish(self, message):
-        self.channel.basic_publish(self.exchange, self.queue, message.encode())
+        self.channel.basic_publish(self.exchange, self.routing_key, message.encode())

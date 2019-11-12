9acd00ab078c35c6e0e16a6959099c90579104b7
Eduardo Neira <edun327@gmail.com>
2019-11-12 19:00:01 -0300

    Analyzed video chunk model in DB

--- big_fiubrother_core/db/__init__.py
+++ big_fiubrother_core/db/__init__.py
@@ -5,4 +5,5 @@ from big_fiubrother_core.db.frame import Frame
 from big_fiubrother_core.db.face import Face
 from big_fiubrother_core.db.face_embedding import FaceEmbedding
 from big_fiubrother_core.db.person import Person
+from big_fiubrother_core.db.analyzed_video_chunk import AnalyzedVideoChunk
 

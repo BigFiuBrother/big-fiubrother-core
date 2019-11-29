6488b3f154161ef8e735df6b4fc494d88cd0548d
Eduardo Neira <edun327@gmail.com>
2019-11-29 00:15:10 -0300

    VideoChunk does not allow null values

--- big_fiubrother_core/db/video_chunk.py
+++ big_fiubrother_core/db/video_chunk.py
@@ -6,9 +6,9 @@ class VideoChunk(Base):
     __tablename__ = 'video_chunks'
 
     id = Column(Integer, primary_key=True)
-    camera_id = Column(String)
-    timestamp = Column(Float)
-    payload = Column(LargeBinary)
+    camera_id = Column(String, nullable=False)
+    timestamp = Column(Float, nullable=False)
+    payload = Column(LargeBinary, nullable=False)
 
     def filename(self):
         return '{}_{}'.format(self.camera_id, self.timestamp)

ea21c8627eb521e8b1afffbdf8c422bb523a42a1
Eduardo Neira <edun327@gmail.com>
2019-11-29 00:15:19 -0300

    Frame does not allow null values

--- big_fiubrother_core/db/frame.py
+++ big_fiubrother_core/db/frame.py
@@ -6,7 +6,7 @@ class Frame(Base):
     __tablename__ = 'frames'
 
     id = Column(Integer, primary_key=True)
-    offset = Column(Integer)
+    offset = Column(Integer, nullable=False)
     video_chunk_id = Column(Integer,
                             ForeignKey('video_chunks.id', deferrable=True),
                             nullable=False)

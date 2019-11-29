9a43780046a0fa896781a686f75a9c2222ec0010
Eduardo Neira <edun327@gmail.com>
2019-11-28 23:53:39 -0300

    Forcing pika 1.1.0. Revert to rabbitmq consumer

--- big_fiubrother_core/message_clients/rabbitmq/consumer.py
+++ big_fiubrother_core/message_clients/rabbitmq/consumer.py
@@ -8,20 +8,23 @@ class Consumer:
         self.queue = configuration['queue']
         self.consume_callback = consume_callback
 
-        self.credentials = pika.PlainCredentials(configuration['username'], configuration['password'])
-        self.parameters = pika.ConnectionParameters(host=self.host, credentials=self.credentials)
+        self.credentials = pika.PlainCredentials(
+            configuration['username'], configuration['password'])
+        self.parameters = pika.ConnectionParameters(
+            host=self.host, credentials=self.credentials)
 
         self.connection = pika.BlockingConnection(self.parameters)
         self.channel = self.connection.channel()
 
-        self.channel.basic_consume(self._process_message, queue=self.queue)
-    
+        self.channel.basic_consume(
+            queue=self.queue, on_message_callback=self._process_message)
+
     def start(self):
         self.channel.start_consuming()
 
     def stop(self):
         self.channel.stop_consuming()
-    
+
     def _process_message(self, ch, method, props, body):
         self.consume_callback(body)
-        ch.basic_ack(delivery_tag = method.delivery_tag)
+        ch.basic_ack(delivery_tag=method.delivery_tag)

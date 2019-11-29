8e3e9d47917077a412cef49c203f9221ecbeed56
Eduardo Neira <edun327@gmail.com>
2019-11-29 00:32:37 -0300

    Added not nullable to processed frame and default to 0 for processed faces count

--- big_fiubrother_core/db/processed_frame.py
+++ big_fiubrother_core/db/processed_frame.py
@@ -10,8 +10,8 @@ class ProcessedFrame(Base):
                       primary_key=True,
                       autoincrement=False)
 
-    total_faces_count = Column(Integer)
-    processed_faces_count = Column(Integer)
+    total_faces_count = Column(Integer, nullable=False)
+    processed_faces_count = Column(Integer, nullable=False, default=0)
 
     def is_completed(self):
         processed_faces_count == total_faces_count

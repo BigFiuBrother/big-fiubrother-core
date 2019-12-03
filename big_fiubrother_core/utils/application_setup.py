61ca9ec9eaaa0c024fe4a1d32e269719edcf2ad1
Eduardo Neira <edun327@gmail.com>
2019-12-02 22:22:27 -0300

    Added logging setup

--- big_fiubrother_core/utils/application_setup.py
+++ big_fiubrother_core/utils/application_setup.py
@@ -1,15 +1,36 @@
 import yaml
 import argparse
+import logging
 import os
 
 
-def setup(application_name, path='config'):
+def setup(application_name, config_path='config', log_path='log'):
     parser = argparse.ArgumentParser(description=application_name)
-    parser.add_argument('environment', type=str, nargs='?', default='development', help='Environment to run applicacion. By default it is development.')
+    parser.add_argument('environment',
+                        type=str,
+                        nargs='?',
+                        default='development',
+                        help="Application environment. By default it's development.")
 
     args = parser.parse_args()
 
-    with open(os.path.join(path, '{}.yml'.format(args.environment.lower()))) as config_file:    
-        configuration = yaml.safe_load(config_file)
+    environment = args.environment.lower()
 
-    return configuration
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    log_format = '%(asctime)s  %(levelname)s  %(process)d  %(thread)d  %(message)s'
+    log_filepath = os.path.join(log_path, '{}.log'.format(environment))
+    logging.basicConfig(level=logging.DEBUG,
+                        format=log_format,
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename=log_filepath)
+
+    logging.getLogger('pika').setLevel(logging.WARNING)
+
+    logging.debug('APPLICATION STARTED')
+
+    configuration_filepath = os.path.join(config_path, '{}.yml'.format(environment))
+
+    with open(configuration_filepath, 'r') as file:
+        return yaml.safe_load(file)

ffcf6886808b5cf214936b086e119639277bf795
Eduardo Neira <edun327@gmail.com>
2019-12-30 01:03:09 -0300

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='big-fiubrother-core',
-   version='0.4.0',
+   version='0.5.0',
    description='Big Fiubrother Core Utilities',
    license="GPLv3",
    long_description=long_description,

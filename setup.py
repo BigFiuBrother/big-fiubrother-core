f8c29d6130ebc18bc58a4e7d476b411e5f57be11
Eduardo Neira <edun327@gmail.com>
2019-12-30 00:49:06 -0300

    Change to requirements to avoid installing opencv if already installed

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# OpenCV should be installed beforehand
+try:
+   import cv2
+except ImportError:
+   requirements.append('opencv-python')
+
 with open("README.md", 'r') as f:
     long_description = f.read()
 
@@ -13,5 +22,6 @@ setup(
    author='Eduardo Neira, Gabriel Gayoso',
    author_email='aneira@fi.uba.ar',
    packages=find_packages(),
+   install_requires=requirements,
    url= 'https://github.com/BigFiuBrother/big-fiubrother-core'
 )

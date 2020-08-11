578f74f1548162746932cea43bc2161544d4ef33
Eduardo Neira <edun327@gmail.com>
2020-08-10 21:26:09 -0300

    Fix

--- big_fiubrother_core/db/video_chunk.py
+++ big_fiubrother_core/db/video_chunk.py
@@ -9,8 +9,8 @@ class VideoChunk(Base):
     id = Column(Integer, primary_key=True)
     camera_id = Column(String, nullable=False)
     timestamp = Column(Float, nullable=False)
-    processed = Column(Boolean, default=True, nullable=False)
-    frames = relationship("Frame", order_by=lambda frame: frame.offset)
+    processed = Column(Boolean, default=False, nullable=False)
+    frames = relationship("Frame")
 
     def filename(self):
         return '{}_{}'.format(self.camera_id, int(self.timestamp))

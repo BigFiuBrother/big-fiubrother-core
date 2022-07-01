113f32a31f3cd40ed32040642eebe06035d00e33
Eduardo Neira <edun327@gmail.com>
2022-07-01 17:20:34 +1000

    Format

--- modules/events/big_fiubrother_core_events/messages/video_chunk_message.py
+++ modules/events/big_fiubrother_core_events/messages/video_chunk_message.py
@@ -1,11 +1,8 @@
-from .abstract_message import AbstractMessage
-
-
-class VideoChunkMessage(AbstractMessage):
+class VideoChunkMessage:
 
     def __init__(self, camera_id, timestamp):
         self.camera_id = camera_id
         self.timestamp = timestamp
 
     def id(self):
-        return "{}-{}".format(self.camera_id, self.timestamp)
+        return f"{self.camera_id}-{self.timestamp}"
